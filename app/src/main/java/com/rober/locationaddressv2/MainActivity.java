914385dcb8eaa51a65102edcba937030c45a7c58
Roberto Sotar <rsotar@gmail.com>
2015-05-30 20:21:41 -0300

    Add Api Key for Google Maps for Android and Find location Functionality

--- app/src/main/java/com/rober/locationaddressv2/MainActivity.java
+++ app/src/main/java/com/rober/locationaddressv2/MainActivity.java
@@ -1,9 +1,25 @@
 package com.rober.locationaddressv2;
 
-import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 
 public class MainActivity extends FragmentActivity {
@@ -57,7 +73,7 @@ public class MainActivity extends FragmentActivity {
 
 
     // An AsyncTask class for accessing the GeoCoding Web Service
-    private class GeocoderTask extends AsyncTask<String, Void, List<Address>>{
+    private class GeocoderTask extends AsyncTask<String, Void, List<Address>> {
 
         @Override
         protected List<Address> doInBackground(String... locationName) {
